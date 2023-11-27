ac9dca985e6ec884859758fa1b5aa4ed84ea7b11
Anna Roses <anna.roses.marti@gmail.com>
2023-11-27 19:11:09 +0100

    Add cors allowed origin to index server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,11 @@
 import morgan from "morgan";
 import express from "express";
+import cors from "cors";
 import app from "./app.js";
 
+const corsPort = process.env.ALLOWED_ORIGIN;
+const corsOptions = { origin: corsPort };
+
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
