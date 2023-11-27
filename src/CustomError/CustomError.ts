9c9c20dd22656d562dd1d9278f287f7ff02781da
Anna Roses <anna.roses.marti@gmail.com>
2023-11-27 19:55:53 +0100

    Add CustomError class

---
+++ src/CustomError/CustomError.ts
@@ -0,0 +1,11 @@
+class CustonError extends Error {
+  constructor(
+    message: string,
+    public readonly statusCode: number,
+    public readonly privateMessage: string,
+  ) {
+    super(message);
+  }
+}
+
+export default CustonError;
