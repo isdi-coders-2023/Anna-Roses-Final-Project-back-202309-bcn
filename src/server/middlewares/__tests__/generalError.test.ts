9f71e78783591162cd9ec357b330d6543c4318c0
Anna Roses <anna.roses.marti@gmail.com>
2023-11-27 22:51:03 +0100

    Add generalError statusCode 500 test

--- src/server/middlewares/__tests__/generalError.test.ts
+++ src/server/middlewares/__tests__/generalError.test.ts
@@ -14,10 +14,10 @@ describe("Given a generalError middleware", () => {
     json: jest.fn().mockReturnThis(),
   };
   const next = jest.fn();
-  const expectedStatusCode = 400;
 
   describe("When it receives a response method status with a 400", () => {
     test("Then it should call the response method status with 400", () => {
+      const expectedStatusCode = 400;
       const customError = new CustomError(errorMessage, expectedStatusCode);
 
       generalError(customError, req as Request, res as Response, next);
@@ -26,8 +26,20 @@ describe("Given a generalError middleware", () => {
     });
   });
 
+  describe("When it receives a response and an error with status code", () => {
+    test("Then it should call the response method status with 500", () => {
+      const expectedStatusCode = 500;
+      const error = new Error("Error with status code");
+
+      generalError(error as CustomError, req as Request, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+  });
+
   describe("When it receives a response with an error with a message 'Error'", () => {
     test("Then it should call the response method json with a 'Private error' message", () => {
+      const expectedStatusCode = 400;
       const privateErrorMessage = "Private error";
       const error = new CustomError(privateErrorMessage, expectedStatusCode);
 
