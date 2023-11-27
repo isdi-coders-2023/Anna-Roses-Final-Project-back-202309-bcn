a00f2428e8064fc0e6817f9f9ad6e29156733952
Anna Roses <anna.roses.marti@gmail.com>
2023-11-27 17:57:07 +0100

    Add startServer port connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import "./server/index.js";
+import { startServer } from "./server/app.js";
 import connectToDatabase from "./database/index.js";
+
+const port = process.env.PORT ?? 400;
 
 if (!process.env.MONGODB_URL) {
   console.log("Missing MongoDB Connection String");
@@ -10,3 +13,5 @@ if (!process.env.MONGODB_URL) {
 const mongoUrl = process.env.MONGODB_URL;
 
 await connectToDatabase(mongoUrl);
+
+startServer(+port);
