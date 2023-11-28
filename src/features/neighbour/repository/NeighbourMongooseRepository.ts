019b3bc1e73b23071e58ffeb6fe6d244d6c73fef
Anna Roses <anna.roses.marti@gmail.com>
2023-11-28 17:02:16 +0100

    Add getNeighbourEndpoint router test and server setup

--- src/features/neighbour/repository/NeighbourMongooseRepository.ts
+++ src/features/neighbour/repository/NeighbourMongooseRepository.ts
@@ -4,7 +4,7 @@ import { type NeighbourRepository } from "./types";
 
 class NeighbourMongooseRepository implements NeighbourRepository {
   public async getNeighbours(): Promise<NeighbourStructure[]> {
-    const neighbours = await Neighbour.find();
+    const neighbours = await Neighbour.find().limit(10);
 
     return neighbours;
   }
