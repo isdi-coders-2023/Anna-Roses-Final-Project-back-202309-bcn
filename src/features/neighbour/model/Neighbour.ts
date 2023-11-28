6c74923b4752331ae5082c4f8e5ed2f7ea340ab2
Anna Roses <anna.roses.marti@gmail.com>
2023-11-28 11:08:40 +0100

    Add Neighbour model

---
+++ src/features/neighbour/model/Neighbour.ts
@@ -0,0 +1,69 @@
+import { Schema, model } from "mongoose";
+import { type NeighbourStructure } from "../types";
+
+const neighbourSchema = new Schema<NeighbourStructure>({
+  name: {
+    type: String,
+    required: true,
+  },
+  floor: {
+    type: String,
+    required: true,
+  },
+  door: {
+    type: String,
+    required: true,
+  },
+  coefficient: {
+    type: Number,
+    required: true,
+  },
+  moneyInFavour: {
+    type: Number,
+    required: true,
+  },
+  ownerFirst: {
+    type: String,
+    required: true,
+  },
+  ownerSecond: {
+    type: String,
+    required: false,
+  },
+  powers: {
+    type: String,
+    required: true,
+  },
+  activityKind: {
+    type: String,
+    required: true,
+  },
+  isFirstResidence: {
+    type: Boolean,
+    required: true,
+  },
+  isRented: {
+    type: Boolean,
+    required: true,
+  },
+  numberOfResidents: {
+    type: Number,
+    required: true,
+  },
+  yearOfPurchase: {
+    type: Number,
+    required: true,
+  },
+  coments: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Neighbour = model("Neighbour", neighbourSchema, "neighbours");
+
+export default Neighbour;
