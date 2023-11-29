6b193f14c5335e3a92acd6bc27245544dc3dd749
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 18:15:06 +0100

    Fixe corsPort to corsUrl

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,11 +3,11 @@ import express from "express";
 import cors from "cors";
 import app from "./app.js";
 import { generalError, notFound } from "./middlewares/errors/generalError.js";
-import neighbourRouter from "../features/neighbour/router/neighbourRouter.js";
+import neighbourRouter from "../features/neighbours/router/neighbourRouter.js";
 import pingRouter from "../features/ping/router/PingRouter.js";
 
-const corsPort = process.env.ALLOWED_ORIGIN;
-const corsOptions = { origin: corsPort };
+const corsUrl = process.env.ALLOWED_ORIGIN;
+const corsOptions = { origin: corsUrl };
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
