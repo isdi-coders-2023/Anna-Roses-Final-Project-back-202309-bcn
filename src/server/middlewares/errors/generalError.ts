8b4656c3e038e2022b230a3053ccceb489fdf9d2
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 18:14:35 +0100

    Add privateError message and test

--- src/server/middlewares/errors/generalError.ts
+++ src/server/middlewares/errors/generalError.ts
@@ -3,11 +3,11 @@ import chalk from "chalk";
 import debugCreator from "debug";
 import CustomError from "../../CustomError/CustomError.js";
 
-const customError = new CustomError("Endpoint not found", 404);
+const debug = debugCreator("neighbours:server:errors:generalError");
 
 export const notFound = (_req: Request, _res: Response, next: NextFunction) => {
-  const debug = debugCreator("neighbours:server:errors:generalError");
-  debug(chalk.red("Endpoint not found"));
+  const customError = new CustomError("Endpoint not found", 404);
+
   next(customError);
 };
 
@@ -18,5 +18,9 @@ export const generalError = (
   _next: NextFunction,
 ) => {
   const statusCode = error.statusCode ?? 500;
+  const privateError = error.privateMessage ?? "Internal server error";
+
+  debug(chalk.red(`error: ${privateError}`));
+
   res.status(statusCode).json({ message: error.message });
 };
