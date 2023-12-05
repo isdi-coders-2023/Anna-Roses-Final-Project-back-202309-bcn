6ebab10693a6c42436a956eada8d5692fcb6a836
Anna Roses <anna.roses.marti@gmail.com>
2023-12-05 10:20:09 +0100

    Add deleteNeighbour and types in neighboursRepository

--- src/features/neighbours/repository/types.ts
+++ src/features/neighbours/repository/types.ts
@@ -2,4 +2,5 @@ import { type NeighbourStructure } from "../types";
 
 export interface NeighboursRepository {
   getNeighbours: () => Promise<NeighbourStructure[]>;
+  deleteNeighbour: (neighbourId: string) => Promise<void>;
 }
