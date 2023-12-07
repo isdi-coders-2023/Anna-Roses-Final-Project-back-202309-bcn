cdec48b356e31364dbc5cbba54251d82f705207a
Anna Roses <anna.roses.marti@gmail.com>
2023-12-07 09:42:35 +0100

    Add schema to addNeighbour

---
+++ src/features/neighbours/schema/neighbourSchema.ts
@@ -0,0 +1,30 @@
+import { Joi, validate } from "express-validation";
+import { type NeighbourStructureWithoutId } from "../types";
+
+const neighbourSchema = {
+  body: Joi.object<NeighbourStructureWithoutId>({
+    name: Joi.string().required(),
+    floor: Joi.string().required(),
+    door: Joi.string().required(),
+    coefficient: Joi.number().required(),
+    moneyInFavour: Joi.number().required(),
+    ownerFirst: Joi.string().required(),
+    ownerSecond: Joi.string().optional(),
+    powers: Joi.string().required(),
+    activityKind: Joi.string().required(),
+    isFirstResidence: Joi.boolean().required(),
+    isRented: Joi.boolean().required(),
+    numberOfResidents: Joi.number().required(),
+    yearOfPurchase: Joi.number().required(),
+    coments: Joi.string().required(),
+    image: Joi.string().required(),
+  }),
+};
+
+const neighbourValidation = validate(
+  neighbourSchema,
+  {},
+  { abortEarly: false },
+);
+
+export default neighbourValidation;
--- src/features/neighbours/types.ts
+++ src/features/neighbours/types.ts
@@ -19,23 +19,7 @@ export interface NeighbourStructure {
   image: string;
 }
 
-export interface NeighbourStructureWithoutId {
-  name: string;
-  floor: string;
-  door: string;
-  coefficient: number;
-  moneyInFavour: number;
-  ownerFirst: string;
-  ownerSecond: string;
-  powers: string;
-  activityKind: string;
-  isFirstResidence: boolean;
-  isRented: boolean;
-  numberOfResidents: number;
-  yearOfPurchase: number;
-  coments: string;
-  image: string;
-}
+export type NeighbourStructureWithoutId = Omit<NeighbourStructure, "_id">;
 
 export type NeighbourRequestWithoutId = Request<
   Record<string, unknown>,
