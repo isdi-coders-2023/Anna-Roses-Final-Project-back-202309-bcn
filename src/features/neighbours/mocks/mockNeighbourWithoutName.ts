75bf1e98e32c09093f39932288f98f3617a26513
Anna Roses <anna.roses.marti@gmail.com>
2023-12-07 17:36:16 +0100

    Add validationError integration test and mockNeighbourWithoutName

---
+++ src/features/neighbours/mocks/mockNeighbourWithoutName.ts
@@ -0,0 +1,18 @@
+import { type NeighbourStructureWithoutName } from "../types";
+
+export const mockNeighbourWithoutName: NeighbourStructureWithoutName = {
+  floor: "0",
+  door: "1",
+  coefficient: 9,
+  moneyInFavour: 246,
+  ownerFirst: "Marta Ibarra Chef",
+  ownerSecond: "Mela Maluenda",
+  powers: "Propietario",
+  activityKind: "Restaurante",
+  isFirstResidence: false,
+  isRented: false,
+  numberOfResidents: 2,
+  yearOfPurchase: 2018,
+  coments: "hola",
+  image: "https://i.ibb.co/N7k0Q4y/minion-chef.webp",
+};
